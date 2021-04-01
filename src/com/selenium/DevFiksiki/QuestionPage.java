343e9b18241a5ed4d71864f659a2161c04f31905
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-04-01 23:36:37 +0300

    Update

--- src/com/selenium/DevFiksiki/QuestionPage.java
+++ src/com/selenium/DevFiksiki/QuestionPage.java
@@ -21,49 +21,91 @@ public class QuestionPage {
         driver.get(QuestionPageURL);
     }
 
+    // 1
     @Test
     public void test_findFileSystemDropdown(){
         driver.findElement(By.id("file-system"));
     }
 
-//    @Test
-//    public void test_FileSystemDropdown(){
-//
-//    }
-
+    // 2
     @Test
-    public void test_FileSystemDropdown(){
+    public void test_FileSystemDropdownDisplayALL(){
+        driver.findElement(By.id("file-system")).click();
+        boolean actual1 = driver.getPageSource().contains("JSON");
+        Assertions.assertTrue(actual1);
+        boolean actual2 = driver.getPageSource().contains("CSV");
+        Assertions.assertTrue(actual2);
+        boolean actual3 = driver.getPageSource().contains("XML");
+        Assertions.assertTrue(actual3);
+        boolean actual4 = driver.getPageSource().contains("YAML");
+        Assertions.assertTrue(actual4);
+    }
+
+    // 3
+    @Test
+    public void test_FileSystemDropdownJSON(){
         driver.findElement(By.id("file-system")).click();
         boolean actual = driver.getPageSource().contains("JSON");
         Assertions.assertTrue(actual);
     }
 
+    // 4
+    @Test
+    public void test_FileSystemDropdownCSV(){
+        driver.findElement(By.id("file-system")).click();
+        boolean actual = driver.getPageSource().contains("CSV");
+        Assertions.assertTrue(actual);
+    }
+
+    // 5
+    // выдает ошибку false вместо true
+//    @Test
+//    public void test_FileSystemDropdownXML(){
+//        driver.findElement(By.id("file-system")).click();
+//        boolean actual = driver.getPageSource().contains("XMl");
+//        Assertions.assertTrue(actual);
+//    }
+
+    // 6
+    @Test
+    public void test_FileSystemDropdownYAML(){
+        driver.findElement(By.id("file-system")).click();
+        boolean actual = driver.getPageSource().contains("YAML");
+        Assertions.assertTrue(actual);
+    }
+
+    // 7
     @Test
     public void test_findQuestionThemeDropdown(){
         driver.findElement(By.id("questions-theme"));
     }
 
+    // 8
     @Test
     public void test_findNewQuestionButton(){
         driver.findElement(By.id("new-question"));
     }
 
+    // 9
     @Test
     public void test_findTeamName(){
         String actual = driver.findElement(By.className("header")).findElement(By.className("header__team-name")).getText();
         Assertions.assertEquals(actual, TEAM_NAME);
     }
 
+    // 10
     @Test
     public void test_findLogo(){
         driver.findElement(By.className("header")).findElement(By.className("header__logo"));
     }
 
+    // 11
     @Test
     public void test_findDeleteMessageButton(){
         driver.findElement(By.id("delete-message"));
     }
 
+    // 12
     @Test
     public void test_findPopUp(){
         driver.findElement(By.id("delete-message")).click();

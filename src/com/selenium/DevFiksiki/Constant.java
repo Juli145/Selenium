d79b78353caedfb92fbdc34b2607ac2ecfaf3cea
Yuliia Frankova <yuliiafrankova@gmail.com>
2021-04-02 11:29:00 +0300

    Update

--- src/com/selenium/DevFiksiki/Constant.java
+++ src/com/selenium/DevFiksiki/Constant.java
@@ -13,5 +13,16 @@ public class Constant {
 
     static final String TEAM_NAME = "DevFiksiki";
 
-    static final String QuestionPageURL = "http://localhost:4200/questions.html";
+    static final String QUESTION_PAGE_URL = "http://localhost:4200/questions.html";
+    static final String FILE_SYSTEM_DROPDOWN_ID = "file-system";
+    static final String QUESTION_THEME_DROPDOWN_ID = "questions-theme";
+    static final String NEW_QUESTION_BUTTON_ID = "new-question";
+    static final String HEADER_CLASS_NAME = "header";
+    static final String TEAM_NAME_CLASS_NAME = "header__team-name";
+    static final String LOGO_CLASS_NAME = "header__logo";
+    static final String DELETE_MESSAGE_BUTTON_ID = "delete-message";
+    static final String POP_UP_ID = "popup";
+    static final String DELETE_QUESTION_CONFIRM_BUTTON_ID = "confirm-btn";
+    static final String DELETE_QUESTION_CANCEL_BUTTON_ID = "cancel-btn";
+    static final String POP_UP_CLOSE_BUTTON_ID = "closePU";
 }
--- src/com/selenium/DevFiksiki/QuestionPage.java
+++ src/com/selenium/DevFiksiki/QuestionPage.java
@@ -18,19 +18,19 @@ public class QuestionPage {
     public void init() {
         System.setProperty("webdriver.chrome.driver", "D://JuliDirectory//Selenium drivers and jars//Drivers//chromedrivers/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get(QuestionPageURL);
+        driver.get(QUESTION_PAGE_URL);
     }
 
     // 1
     @Test
     public void test_findFileSystemDropdown(){
-        driver.findElement(By.id("file-system"));
+        driver.findElement(By.id(FILE_SYSTEM_DROPDOWN_ID));
     }
 
     // 2
     @Test
     public void test_FileSystemDropdownDisplayALL(){
-        driver.findElement(By.id("file-system")).click();
+        driver.findElement(By.id(FILE_SYSTEM_DROPDOWN_ID)).click();
         boolean actual1 = driver.getPageSource().contains("JSON");
         Assertions.assertTrue(actual1);
         boolean actual2 = driver.getPageSource().contains("CSV");
@@ -41,75 +41,126 @@ public class QuestionPage {
         Assertions.assertTrue(actual4);
     }
 
-    // 3
-    @Test
-    public void test_FileSystemDropdownJSON(){
-        driver.findElement(By.id("file-system")).click();
-        boolean actual = driver.getPageSource().contains("JSON");
-        Assertions.assertTrue(actual);
-    }
-
-    // 4
-    @Test
-    public void test_FileSystemDropdownCSV(){
-        driver.findElement(By.id("file-system")).click();
-        boolean actual = driver.getPageSource().contains("CSV");
-        Assertions.assertTrue(actual);
-    }
-
-    // 5
-    // выдает ошибку false вместо true
+//    // 3
 //    @Test
-//    public void test_FileSystemDropdownXML(){
-//        driver.findElement(By.id("file-system")).click();
-//        boolean actual = driver.getPageSource().contains("XMl");
+//    public void test_FileSystemDropdownJSON(){
+//        driver.findElement(By.id(FILE_SYSTEM_DROPDOWN_ID)).click();
+//        boolean actual = driver.getPageSource().contains("JSON");
 //        Assertions.assertTrue(actual);
 //    }
-
-    // 6
-    @Test
-    public void test_FileSystemDropdownYAML(){
-        driver.findElement(By.id("file-system")).click();
-        boolean actual = driver.getPageSource().contains("YAML");
-        Assertions.assertTrue(actual);
-    }
+//
+//    // 4
+//    @Test
+//    public void test_FileSystemDropdownCSV(){
+//        driver.findElement(By.id(FILE_SYSTEM_DROPDOWN_ID)).click();
+//        boolean actual = driver.getPageSource().contains("CSV");
+//        Assertions.assertTrue(actual);
+//    }
+//
+//    // 5
+//    @Test
+//    public void test_FileSystemDropdownXML(){
+//        driver.findElement(By.id(FILE_SYSTEM_DROPDOWN_ID)).click();
+//        boolean actual = driver.getPageSource().contains("XML");
+//        Assertions.assertTrue(actual);
+//    }
+//
+//    // 6
+//    @Test
+//    public void test_FileSystemDropdownYAML(){
+//        driver.findElement(By.id(FILE_SYSTEM_DROPDOWN_ID)).click();
+//        boolean actual = driver.getPageSource().contains("YAML");
+//        Assertions.assertTrue(actual);
+//    }
 
     // 7
     @Test
     public void test_findQuestionThemeDropdown(){
-        driver.findElement(By.id("questions-theme"));
+        driver.findElement(By.id(QUESTION_THEME_DROPDOWN_ID));
     }
 
     // 8
     @Test
-    public void test_findNewQuestionButton(){
-        driver.findElement(By.id("new-question"));
+    public void test_QuestionThemeDropdownDisplayALL(){
+        driver.findElement(By.id(QUESTION_THEME_DROPDOWN_ID));
+        boolean actual1 = driver.getPageSource().contains("Theme 1");
+        Assertions.assertTrue(actual1);
+        boolean actual2 = driver.getPageSource().contains("Theme 2");
+        Assertions.assertTrue(actual2);
+        boolean actual3 = driver.getPageSource().contains("Theme 3");
+        Assertions.assertTrue(actual3);
+        boolean actual4 = driver.getPageSource().contains("Theme 4");
+        Assertions.assertTrue(actual4);
+        boolean actual5 = driver.getPageSource().contains("Theme 5");
+        Assertions.assertTrue(actual5);
     }
+
+//    @Test
+//    public void test_QuestionThemeDropdownTheme1(){
+//        driver.findElement(By.id(QUESTION_THEME_DROPDOWN_ID)).click();
+//        boolean actual = driver.getPageSource().contains("Theme 1");
+//        Assertions.assertTrue(actual);
+//    }
 
     // 9
     @Test
-    public void test_findTeamName(){
-        String actual = driver.findElement(By.className("header")).findElement(By.className("header__team-name")).getText();
-        Assertions.assertEquals(actual, TEAM_NAME);
+    public void test_findNewQuestionButton(){
+        driver.findElement(By.id(NEW_QUESTION_BUTTON_ID));
     }
 
     // 10
     @Test
-    public void test_findLogo(){
-        driver.findElement(By.className("header")).findElement(By.className("header__logo"));
+    public void test_findTeamName(){
+        String actual = driver.findElement(By.className(HEADER_CLASS_NAME)).findElement(By.className(TEAM_NAME_CLASS_NAME)).getText();
+        Assertions.assertEquals(actual, TEAM_NAME);
     }
 
     // 11
     @Test
-    public void test_findDeleteMessageButton(){
-        driver.findElement(By.id("delete-message"));
+    public void test_findLogo(){
+        driver.findElement(By.className(HEADER_CLASS_NAME)).findElement(By.className(LOGO_CLASS_NAME));
     }
 
     // 12
     @Test
+    public void test_findDeleteMessageButton(){
+        driver.findElement(By.id(DELETE_MESSAGE_BUTTON_ID));
+    }
+
+    // 13
+    @Test
     public void test_findPopUp(){
-        driver.findElement(By.id("delete-message")).click();
-        driver.findElement(By.className("popup"));
+        driver.findElement(By.id(DELETE_MESSAGE_BUTTON_ID)).click();
+        driver.findElement(By.id(POP_UP_ID));
+    }
+
+    // 14
+    @Test
+    public void test_MW_DeleteQuestion(){
+        driver.findElement(By.id(DELETE_MESSAGE_BUTTON_ID)).click();
+        boolean actual = driver.getPageSource().contains("Are you sure you want to delete this question?");
+        Assertions.assertTrue(actual);
+    }
+
+    // 15
+    @Test
+    public void test_find_MW_DeleteQuestion_ConfirmButton(){
+        driver.findElement(By.id(DELETE_MESSAGE_BUTTON_ID)).click();
+        driver.findElement(By.id(DELETE_QUESTION_CONFIRM_BUTTON_ID)).click();
+    }
+
+    // 16
+    @Test
+    public void test_find_MW_DeleteQuestion_CancelButton(){
+        driver.findElement(By.id(DELETE_MESSAGE_BUTTON_ID)).click();
+        driver.findElement(By.id(DELETE_QUESTION_CANCEL_BUTTON_ID)).click();
+    }
+
+    // 17
+    @Test
+    public void test_find_MW_DeleteQuestion_CloseButton(){
+        driver.findElement(By.id(DELETE_MESSAGE_BUTTON_ID)).click();
+        driver.findElement(By.id(POP_UP_CLOSE_BUTTON_ID)).click();
     }
 
     @After
